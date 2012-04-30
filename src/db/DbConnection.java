6bdd01f0200010de65d2a1ec9b4547d480acab6e
Braden Simpson <braden@uvic.ca>
2012-04-30 15:50:40 -0700

    Added postgresql jdbc connector, started writing DbConnection class

---
+++ src/db/DbConnection.java
@@ -0,0 +1,98 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+import scm2pgsql.Resources;
+
+public class DbConnection {
+	public static Connection conn = null;
+	public static DbConnection ref = null;
+	public static String DriverString = "org.postgresql.jdbc4";
+	private DbConnection() 
+	{
+		try 
+		{
+			Class.forName("org.postgresql.Driver").newInstance();
+		} 
+		catch (InstantiationException e) 
+		{
+			e.printStackTrace();
+		} 
+		catch (IllegalAccessException e) 
+		{
+			e.printStackTrace();
+		} 
+		catch (ClassNotFoundException e) 
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	public static DbConnection getInstance() 
+	{
+		if (ref == null)
+			return (ref = new DbConnection());
+		else
+			return ref;
+	}
+	
+	/**
+	 * Executes a string of SQL on the current databse
+	 * @param sql
+	 * @return true if successful
+	 */
+	public boolean exec(String sql)
+	{
+		return true;
+	}
+	
+	/**
+	 * Connects to the given database.  
+	 * @param connectionString
+	 * @return true if successful
+	 */
+	public boolean connect(String connectionString)
+	{
+		try {
+			conn = DriverManager.getConnection(connectionString, Resources.dbUsername, Resources.dbPassword);
+		} 
+		catch (SQLException e) 
+		{
+			e.printStackTrace();
+		}
+		return true;
+	}
+	
+	/**
+	 * Creates a db on the current connection.
+	 * @param dbName
+	 * @return true for success
+	 */
+	public boolean createDB(String dbName)
+	{
+		PreparedStatement s;
+		try {
+			s = conn.prepareStatement("CREATE DATABASE " + dbName + ";"); // TODO SQL escaping?
+			s.execute();
+			return true;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+	public boolean close()
+	{
+		try {
+			conn.close();
+			return true;
+		}
+		catch (SQLException e)
+		{
+			e.printStackTrace();
+			return false;
+		}
+	}
+}
--- src/scm2pgsql/Main.java
+++ src/scm2pgsql/Main.java
@@ -1,21 +1,40 @@
 package scm2pgsql;
 
+import java.io.IOException;
+
+import org.eclipse.jgit.errors.MissingObjectException;
+
 import git.Git;
 
 public class Main {
+	public static Git gitParser = new Git();
 	public static void main(String[] args)
 	{
 		System.out.println("Scm2Pgsql tool developed by eggnet.");
 		try {
-			if (args.length < 2)
+			System.out.println(args.length);
+			if (args.length < 1)
 			{
 				throw new ArrayIndexOutOfBoundsException();
 			}
 			else
 			{
-				if (args[1].equals("--convert"))
+				if (args[0].equals("--convert"))
 				{
 					// TODO @jordan add the converter code here.
+					return;
+				}
+				else
+				{
+					try {
+						gitParser.parseRepo(".git");
+					} catch (MissingObjectException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
 				}
 			}
 		}
