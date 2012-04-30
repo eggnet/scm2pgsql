17178ff0d5896a295601e6c8ee85e15c2ec9dd66
Jordan Ell <Jordan@bellydancing.(none)>
2012-04-30 15:55:31 -0700

    Added the code to run the shell script and handle the main input of the Java program for SVN conversions

--- src/scm2pgsql/Main.java
+++ src/scm2pgsql/Main.java
@@ -1,6 +1,9 @@
 package scm2pgsql;
 
+import java.io.IOException;
+
 import git.Git;
+import converters.SVNConverter;
 
 public class Main {
 	public static void main(String[] args)
@@ -15,13 +18,31 @@ public class Main {
 			{
 				if (args[1].equals("--convert"))
 				{
-					// TODO @jordan add the converter code here.
+					if(args[2].equals("SVN") || args[2].equals("svn"))
+					{
+						SVNConverter converter = SVNConverter.getInstance();
+						if(converter.Convert(args[3]))
+							System.out.println("Conversion from SVN to Git was successfull");
+						else
+						{
+							System.out.println("Conversion from SVN to Git has failed");
+							throw new IOException();
+						}
+					}
+					else if(args[2].equals("CVS") || args[2].equals("cvs"))
+					{
+						// TODO @triet add the CVS converter stuff here.
+					}
 				}
 			}
 		}
 		catch (ArrayIndexOutOfBoundsException e)
 		{
 			System.out.println("Usage scm2pgsql [--convert repositoryType] <input repository>");
+		}
+		catch (IOException e)
+		{
+			System.out.println("The process will terminate due to the failed conversion.");
 		}
 	}
 }
