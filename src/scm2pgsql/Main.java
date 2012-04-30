ba7e36a1aad3f8ddd621625c2f6e60332c1a4bb8
Braden Simpson <braden@uvic.ca>
2012-04-30 16:01:08 -0700

    Gitignore

--- src/scm2pgsql/Main.java
+++ src/scm2pgsql/Main.java
@@ -29,10 +29,8 @@ public class Main {
 					try {
 						gitParser.parseRepo(".git");
 					} catch (MissingObjectException e) {
-						// TODO Auto-generated catch block
 						e.printStackTrace();
 					} catch (IOException e) {
-						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
 				}
