0d0e34b46004fab2c6d0d51ab86e24d75b0b5251
Braden Simpson <braden@uvic.ca>
2012-04-30 14:25:30 -0700

    Added some tests to git.java

--- src/git/Git.java
+++ src/git/Git.java
@@ -1,14 +1,49 @@
 package git;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 
+import org.eclipse.jgit.errors.MissingObjectException;
+import org.eclipse.jgit.lib.Constants;
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.ObjectLoader;
 import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.lib.RepositoryBuilder;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.RevTree;
+import org.eclipse.jgit.revwalk.RevWalk;
+import org.eclipse.jgit.treewalk.TreeWalk;
+import org.eclipse.jgit.treewalk.filter.PathFilter;
 
 public class Git {
-	public static File repoFile;
-	public static Repository repo;
-	public void parseRepo(String repoDir)
+	public File repoDir;
+	public RepositoryBuilder repoBuilder;
+	public Repository repoFile;
+	public void parseRepo(String inDir) throws MissingObjectException, IOException
 	{
-		 
+		repoDir = new File(inDir);
+		repoBuilder.findGitDir(repoDir);
+		repoFile = this.repoBuilder.build();
+		// find the HEAD
+		ObjectId lastCommitId = repoFile.resolve(Constants.HEAD);
+		// now we have to get the commit
+		RevWalk revWalk = new RevWalk(repoFile);
+		RevCommit commit = revWalk.parseCommit(lastCommitId);
+		// and using commit's tree find the path
+		RevTree tree = commit.getTree();
+		TreeWalk treeWalk = new TreeWalk(repoFile);
+		treeWalk.addTree(tree);
+		treeWalk.setFilter(PathFilter.create(""));
+		if (!treeWalk.next()) {
+		  return;
+		}
+		ObjectId objectId = treeWalk.getObjectId(0);
+		ObjectLoader loader = repoFile.open(objectId);
+
+		// and then one can use either
+		InputStream in = loader.openStream();
+		// or
+		loader.copyTo(System.out);
 	}
 }
--- src/scm2pgsql/Main.java
+++ src/scm2pgsql/Main.java
@@ -6,6 +6,22 @@ public class Main {
 	public static void main(String[] args)
 	{
 		System.out.println("Scm2Pgsql tool developed by eggnet.");
-		
+		try {
+			if (args.length < 2)
+			{
+				throw new ArrayIndexOutOfBoundsException();
+			}
+			else
+			{
+				if (args[1].equals("--convert"))
+				{
+					// TODO @jordan add the converter code here.
+				}
+			}
+		}
+		catch (ArrayIndexOutOfBoundsException e)
+		{
+			System.out.println("Usage scm2pgsql [--convert repositoryType] <input repository>");
+		}
 	}
 }
