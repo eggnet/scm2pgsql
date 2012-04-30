8757715d53ebeafa313b2acea22a8a777a97c321
Braden Simpson <braden@uvic.ca>
2012-04-30 13:50:58 -0700

    Git class changes

--- src/git/Git.java
+++ src/git/Git.java
@@ -1,6 +1,14 @@
 package git;
 
-import org.eclipse.jgit.*;
+import java.io.File;
+
+import org.eclipse.jgit.lib.Repository;
 
 public class Git {
+	public static File repoFile;
+	public static Repository repo;
+	public void parseRepo(String repoDir)
+	{
+		 
+	}
 }
